37e6e8a8b5f65cd2f1bd26d80fdb963b5cbe7b88
jankavka <jankavka@seznam.cz>
2025-12-24 08:15:17 +0100

    Added static resource handler

---
+++ ReservationAppServer/src/main/java/cz/reservation/configuration/StaticResourcesConfig.java
@@ -0,0 +1,29 @@
+package cz.reservation.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class StaticResourcesConfig implements WebMvcConfigurer {
+
+    @Value("${file.directory}")
+    private String fileDirectory;
+
+
+    /**
+     * Adds resource handlers to application. Method addResourceHandler() determines a URL where we find
+     * our resource. Method addResourceLocation is exact location of resource on server
+     *
+     * @param registry param where resource infos are set
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry.addResourceHandler("/files/**")
+                .addResourceLocations("file:" + fileDirectory + "/");
+    }
+
+
+}
