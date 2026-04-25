2c1f6dcc333595a74333af2898ad57adba09a86c
jankavka <jankavka@seznam.cz>
2026-04-25 16:57:13 +0200

    New Routes
    - Updated .gitignore
    - Added new Routes to App.tsx
    - Added Menu to PublicLayout.tsx
    - Added Home.tsx, Reservation.tsx, Contact.tsx, AboutUs.tsx, Courts.tsx

--- ReservationAppClient/src/layouts/publicLayout.tsx
+++ ReservationAppClient/src/layouts/publicLayout.tsx
@@ -1,6 +1,10 @@
 import NavLinks from "../components/NavLinks";
 import { Route, Routes } from "react-router";
 import Home from "../pages/Home";
+import Courts from "../pages/Courts";
+import Reservation from "../pages/reservation";
+import AboutUs from "../pages/AboutUs";
+import Contacts from "../pages/Contacts";
 
 const PublicLayout = () => {
   return (
@@ -8,7 +12,11 @@ const PublicLayout = () => {
       <NavLinks />
 
       <Routes>
-        <Route path={"/"} element={<Home />} />
+        <Route index element={<Home />} />
+        <Route path="/rezervace" element={<Reservation />} />
+        <Route path="/kurty" element={<Courts/>}/>
+        <Route path="/o-nas" element={<AboutUs/>}/>
+        <Route path="/kontakty" element={<Contacts/>}/>
       </Routes>
     </div>
   );
---
+++ ReservationAppClient/src/pages/Courts.tsx
@@ -0,0 +1,15 @@
+import { Container } from "react-bootstrap"
+
+const Courts = () => {
+
+    return(
+        <>
+        <Container>
+            <h1 className="text-center">Kurty</h1>
+        </Container>
+        </>
+    )
+
+}
+
+export default Courts
